19d5d88f22580d904f858ddc62d1e65ff1cb08ab
VincenzoConte <vincenzoconte@hotmail.com>
2020-05-05 13:15:47 +0200

    added post request

--- server.js
+++ server.js
@@ -5,14 +5,22 @@ var port = process.env.PORT || 1337;
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 const fs = require('fs');
 
-//register a new user with a random generated ID and the name received
-app.all('/register', function (req, res) {
-    var name = req.query.name;
-    if (!name || name == null || name == undefined) {
+
+//get requests implemented only for testing
+
+
+//register a new user with a random generated ID and the username received
+app.get('/register', function (req, res) {
+    var username = req.query.username;
+    if (!username || username == null || username == undefined) {
         res.writeHead(422, { 'Content-Type': 'text/plain' });
-        res.end("missing name");
+        res.end("missing username");
         return;
     }
 
@@ -21,8 +29,14 @@ app.all('/register', function (req, res) {
     var id = generateUserID();
     let user = {
         id: id,
-        name: name,
+        username: username,
     };
+
+    //check if there is a facebook id in the request
+    var facebookID = req.query.facebookid;
+    if (facebookID && facebookID != null && facebookID != undefined)
+        user.facebookID = facebookID;
+
     users.push(user);
 
     //writing the updated list
@@ -36,8 +50,50 @@ app.all('/register', function (req, res) {
         }
 
         // Success
+        res.set("id", id);
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end("your id: " + id);
+        res.end("user registration completed");
+    });
+
+});
+
+app.post('/register', function (req, res) {
+    var username = req.body.username;
+    if (!username || username == null || username == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("missing username");
+        return;
+    }
+
+    //getting users file and adding the new user
+    const users = require("./Users");
+    var id = generateUserID();
+    let user = {
+        id: id,
+        username: username,
+    };
+
+    //check if there is a facebook id in the request
+    var facebookID = req.body.facebookid;
+    if (facebookID && facebookID != null && facebookID != undefined)
+        user.facebookID = facebookID;
+
+    users.push(user);
+
+    //writing the updated list
+    fs.writeFile("Users.json", JSON.stringify(users, null, 2), err => {
+
+        // Checking for errors
+        if (err) {
+            res.writeHead(501, { 'Content-Type': 'text/plain' });
+            res.end("server encountered an error");
+            return;
+        }
+
+        // Success
+        res.set("id", id);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("user registration completed");
     });
 
 });
@@ -45,8 +101,8 @@ app.all('/register', function (req, res) {
 
 
 //associate a facebook ID with an existing user
-app.all('/associatefacebook', (req, res) => {
-    var id = req.query.id;
+app.get('/associatefacebook', (req, res) => {
+    var id = req.query.userid;
     if (!id || id == null || id == undefined) {
         res.writeHead(422, { 'Content-Type': 'text/plain' });
         res.end("missing parameter");
@@ -90,12 +146,57 @@ app.all('/associatefacebook', (req, res) => {
     });
 });
 
+app.post('/associatefacebook', (req, res) => {
+    var id = req.body.userid;
+    if (!id || id == null || id == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("missing parameter");
+        return;
+    }
+
+    const users = require("./Users");
+
+    //check if a user with the id received exists
+    var userFound = users.find(function (user) {
+        return user.id == id;
+    });
+
+    if (!userFound || userFound == null || userFound == undefined) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("user not found");
+        return;
+    }
+
+
+    var facebookID = req.body.facebookid;
+    if (!facebookID || facebookID == null || facebookID == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("missing parameter");
+        return;
+    }
+
+    //updating the user with the facebook ID and writing on file
+    userFound.facebookID = facebookID;
+    fs.writeFile("Users.json", JSON.stringify(users, null, 2), err => {
+        // Checking for errors
+        if (err) {
+            res.writeHead(501, { 'Content-Type': 'text/plain' });
+            res.end("server encountered an error");
+            return;
+        }
+
+        //association completed
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("your facebook account has been associated");
+    });
+});
+
 
 
 //start new session for a user at a location
-app.all('/startsession', (req, res) => {
+app.get('/startsession', (req, res) => {
     var userid = req.query.userid;
-    var locationID = req.query.locationid;
+    var locationID = req.query.locationnid;
     if (!userid || userid == null || userid == undefined || !locationID || locationID == null || locationID == undefined) {
         res.writeHead(422, { 'Content-Type': 'text/plain' });
         res.end("invalid parameters");
@@ -149,6 +250,70 @@ app.all('/startsession', (req, res) => {
         }
 
         //success
+        res.set("sessionid", sessionid);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("session started");
+    });
+
+});
+
+app.post('/startsession', (req, res) => {
+    var userid = req.body.userid;
+    var locationID = req.body.locationid;
+    if (!userid || userid == null || userid == undefined || !locationID || locationID == null || locationID == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("invalid parameters");
+        return;
+    }
+
+    const users = require('./Users');
+
+    //check if a user with the id received exists
+    let elementfound = users.some((user) => {
+        return user.id == userid
+    });
+
+    if (!elementfound) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("user not found");
+        return;
+    }
+
+    const sessions = require('./Sessions');
+
+    //check if the session already exists
+    elementfound = sessions.some((session) => {
+        return session.userid == userid && session.locationid == locationID;
+    });
+    if (elementfound) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("session already done");
+        return;
+    }
+
+    //adding the session
+    var sessionid = generateSessionID();
+    let session = {
+        sessionid: sessionid,
+        userid: userid,
+        locationid: locationID,
+        active: true
+    };
+
+    sessions.push(session);
+
+
+    //writing on file
+    fs.writeFile("Sessions.json", JSON.stringify(sessions, null, 2), err => {
+        if (err) {
+            //error handling
+            res.writeHead(501, { 'Content-Type': 'text/plain' });
+            res.end("server encountered an error");
+            return;
+        }
+
+        //success
+        res.set("sessionid", sessionid);
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end("session started");
     });
@@ -158,7 +323,7 @@ app.all('/startsession', (req, res) => {
 
 
 //end an active session and adds the score to it
-app.all('/endsession', (req, res) => {
+app.get('/endsession', (req, res) => {
     var userid = req.query.userid;
     var sessionid = req.query.sessionid;
     if (!userid || userid == null || userid == undefined || !sessionid || sessionid == null || sessionid == undefined) {
@@ -207,6 +372,57 @@ app.all('/endsession', (req, res) => {
     });
 
 });
+
+app.post('/endsession', (req, res) => {
+    var userid = req.body.userid;
+    var sessionid = req.body.sessionid;
+    if (!userid || userid == null || userid == undefined || !sessionid || sessionid == null || sessionid == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("invalid parameters");
+        return;
+    }
+
+    const sessions = require('./Sessions');
+
+    //check if the session exists and is associated with the given user
+    let sessionfound = sessions.find((session) => {
+        return session.sessionid == sessionid && session.userid == userid;
+    });
+
+    if (!sessionfound || sessionfound == null || sessionfound == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("this session doesn't exists");
+        return;
+    }
+
+
+    var score = req.body.score;
+    if (!score || score == null || score == undefined) {
+        res.writeHead(422, { 'Content-Type': 'text/plain' });
+        res.end("need the score to end the session");
+        return;
+    }
+
+    //marking session done and adding score
+    sessionfound.active = false;
+    sessionfound.score = score;
+
+    //writing on file
+    fs.writeFile("Sessions.json", JSON.stringify(sessions, null, 2), err => {
+        if (err) {
+            //error handling
+            res.writeHead(501, { 'Content-Type': 'text/plain' });
+            res.end("server encountered an error");
+            return;
+        }
+
+        //success
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end("session ended");
+    });
+
+});
+
 
 
 app.listen(port, function () {
